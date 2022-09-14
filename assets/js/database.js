7400638ef20c095f877f578c88003d26dbeb6fe2
Vitor Galdino <httpvittor@gmail.com>
2022-09-14 11:31:41 -0300

    final project

---
+++ assets/js/database.js
@@ -0,0 +1,94 @@
+// Banco de dados dos produtos
+
+const data = [
+  {
+    id: 1,
+    img: "assets/img/jaqueta.svg",
+    nameItem: "Lightweight Jacket",
+    description:
+      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 2,
+    img: "assets/img/gorro.svg",
+    nameItem: "Black Hat",
+    description:
+      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+  {
+    id: 3,
+    img: "assets/img/mascara.svg",
+    nameItem: "Mask",
+    description:
+      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
+    value: 40,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+  {
+    id: 4,
+    img: "assets/img/camiseta_preta.svg",
+    nameItem: "T-Shirt",
+    description:
+      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 5,
+    img: "assets/img/camiseta_branca.svg",
+    nameItem: "Short-Sleeve T-Shirt",
+    description:
+      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 6,
+    img: "assets/img/moletom.svg",
+    nameItem: "Champion Packable Jacket",
+    description:
+      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 7,
+    img: "assets/img/nike-air-force.jpg",
+    nameItem: "Tênis Nike Air Force 1",
+    description:
+      "Leve seu jogo para outro patamar com esta nova opção inspirada nas suas próximas férias...",
+    value: 639,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Calçados"],
+  },
+  {
+    id: 8,
+    img: "assets/img/nike-premium.jpg",
+    nameItem: "Tênis Nike Air Force 1",
+    description:
+      "Prepare-se para sua aventura. Este AF1 dá um toque premium ao que você conhece melhor...",
+    value: 899,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Calçados"],
+  },
+  {
+    id: 9,
+    img: "assets/img/jordan.jpg",
+    nameItem: "Tênis Air Jordan 1 Mid",
+    description:
+      "Destaque-se com o estilo atemporal do basquete. O couro premium é polido, envernizado...",
+    value: 999,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Calçados"],
+  },
+];
---
+++ assets/js/index.js
@@ -0,0 +1,236 @@
+const extendCart = pxValue => document.querySelector(".cart").style.height = `${pxValue}px`;
+
+extendCart("220");
+// 320 max
+
+const animation = (tagName, pxValue, position) => {
+    document.querySelector(tagName).style.transition = `all .3s`;
+    document.querySelector(tagName).style.transform = `translate${position}(${pxValue}px)`;
+}
+
+// theme switcher
+const switcher = document.querySelector(".switcher");
+switcher.addEventListener("change", themeSwitcher);
+let i = 0
+function themeSwitcher(event) {
+    if (event.target.checked) {
+        document.querySelector("body").style.transition = "all .4s"
+        document.querySelector("body").style.background = "#121212"
+        document.querySelector("body").style.color = "#D7D7D7"
+        document.querySelector("header").style.background = "#1F1F1F"
+        document.querySelector("ul").className = "white-color";
+    } else {
+        document.querySelector("body").style.background = "#fff"
+        document.querySelector("body").style.color = "#333333"
+        document.querySelector("header").style.background = "#fff"
+        document.querySelector("ul").className = "black-color";
+    }
+}
+
+// menu functions, render categories
+const nav = document.querySelector("nav");
+nav.addEventListener("click", renderProductCategory);
+function renderProductCategory(event = category) {
+    const obj = [];
+    const selected = document.querySelector(".selected");
+    let categoryName = event.path[0].textContent;
+
+    for (let i = 0; i < data.length; i++) {
+        if (categoryName == "Todos") {
+            obj.push(data[i]);
+            selected.classList.remove("selected");
+            event.path[0].classList.add("selected");
+        } else if (data[i].tag[0] == categoryName) {
+            obj.push(data[i]);
+            selected.classList.remove("selected");
+            event.path[0].classList.add("selected");
+        }
+    }
+    obj.length > 0 ? renderProducts(obj) : false;
+}
+
+// render products in the showcase
+const ulProducts = document.querySelector(".products");
+function renderProducts(obj = data) {
+    ulProducts.innerHTML = "";
+    for (let i = 0; i < obj.length; i++) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+        <li id="${obj[i].id}">
+            <img src="${obj[i].img}" alt="${obj[i].nameItem} image">
+            <div class="info-product">
+                <span class="tag">${obj[i].tag[0]}</span>
+                <h3 class="product-title">${obj[i].nameItem}</h3>
+                <p>${obj[i].description}</p>
+                <span>R$ ${obj[i].value},00</span>
+                <button class="buy-btn">Adicionar ao carrinho</button>
+            </div>
+        </li>
+        `;
+        ulProducts.append(card);
+    }
+    titleLengthIsValid();
+}
+renderProducts()
+
+function titleLengthIsValid() {
+    const productTitle = document.querySelectorAll(".product-title");
+    for (let i = 0; i < productTitle.length; i++) {
+        if (productTitle[i].textContent.length > 22) {
+            productTitle[i].style.fontSize = "16px";
+        }
+    }
+}
+
+// search and render products / search functions
+const input = document.querySelector("#search-product");
+input.addEventListener('input', capitalizeInputWord);
+
+function capitalizeInputWord() {
+    renderSearchedProduct(this.value)
+}
+
+function renderSearchedProduct(str) {
+    const obj = [];
+    for (let i = 0; i < data.length; i++) {
+        let nameProduct = data[i].nameItem.toLowerCase();
+        if (nameProduct.includes(str.toLowerCase())) {
+            obj.push(data[i]);
+        }
+    }
+    renderProducts(obj);
+    document.querySelector(".selected").classList.remove("selected");
+    document.querySelector("a").classList.add("selected");
+}
+
+// add products to cart / cart functions
+const productUl = document.querySelector(".products")
+const cart = document.querySelector(".cart");
+let countProduct = 0;
+let sumPrice = 0;
+
+productUl.addEventListener("click", sendProductToCart);
+function sendProductToCart(event) {
+    if (event.path[0].className === "buy-btn") {
+        cart.appendChild(productsSelectedUl);
+
+        extendCart("280");
+        renderProductsAtCart(event.path[2].id);
+        renderAmountAndPrice();
+    }
+}
+
+const productsSelectedUl = document.createElement("ul");
+productsSelectedUl.classList.add("products-selected");
+
+function renderProductsAtCart(idProduct) {
+    const elementId = parseInt(idProduct);
+
+    if (!verifyDuplicateProducts(elementId)) {
+        popUpWarning(true, "Item adicionado!");
+    } else {
+        popUpWarning(false, "Item já adicionado!");
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id === elementId && !verifyDuplicateProducts(elementId)) {
+            countProduct++;
+            sumPrice += data[i].value;
+            const li = document.createElement("li");
+            li.setAttribute("id", `cart-${data[i].id}`)
+            li.innerHTML = `
+            <div class="product-img">
+                <img src="${data[i].img}" alt="${data[i].nameItem} image">
+            </div>
+            <div class="product-info">
+                <h3>${data[i].nameItem}</h3>
+                <span>R$ ${data[i].value},00</span>
+                <button class="remove-btn"><span>Remover produto</span></button>
+            </div>
+            `;
+            productsSelectedUl.appendChild(li);
+            animation(`#cart-${data[i].id}`, -270, "x");
+
+            setTimeout(function () {
+                animation(`#cart-${data[i].id}`, 0, "x");
+            }, 1)
+        }
+    }
+    cartWarningBox();
+}
+
+function verifyDuplicateProducts(id) {
+    let idCart = document.querySelector(`#cart-${id}`);
+    if (idCart == null) return false;
+    return true;
+}
+
+const footerCart = document.createElement("div");
+footerCart.classList.add("footer-cart");
+function renderAmountAndPrice() {
+    footerCart.innerHTML = `
+    <div class="amount-box">
+        <h4>Quantidade:</h4>
+        <span id="count-product">${countProduct}</span>
+    </div>
+    <div class="total">
+        <h4>Total:</h4>
+        <span id="price-value">R$ ${sumPrice},00</span>
+    </div>
+    <div class="payment">
+        <button><span>Comprar</span></button>
+    </div>
+    `;
+    cart.appendChild(footerCart);
+}
+
+function popUpWarning(bool, warning) {
+    const divPopUp = document.querySelector(".pop-up");
+
+    if (bool) {
+        divPopUp.innerHTML = `<span><img draggable="false" src="assets/img/check.png">${warning}</span>`;
+        animation(".pop-up", 70, "y");
+        setTimeout(function () {
+            animation(".pop-up", -40, "y");
+        }, 700)
+    } else {
+        divPopUp.innerHTML = `<span><img draggable="false" src="assets/img/error.png">${warning}</span>`;
+        animation(".pop-up", 70, "y");
+        setTimeout(function () {
+            animation(".pop-up", -40, "y");
+        }, 900)
+    }
+}
+
+cart.addEventListener("click", removeCartProducts);
+function removeCartProducts(event) {
+    if (event.path[1].className === "remove-btn") {
+        countProduct--;
+        sumPrice -= parseInt(event.path[2].children[1].textContent.replace(",00", "").substr(3));
+        document.querySelector("#count-product").innerHTML = countProduct;
+        document.querySelector("#price-value").innerHTML = `R$ ${sumPrice},00`;
+
+        animation(`#${event.path[3].id}`, -270, "x");
+        setTimeout(function () {
+            event.path[3].remove();
+        }, 300)
+        cartWarningBox();
+    }
+}
+
+function cartWarningBox() {
+    const warningBox = document.createElement("div");
+    warningBox.classList.add("warning-cart");
+    if (countProduct === 0) {
+        document.querySelector(".footer-cart").remove();
+        document.querySelector(".products-selected").remove();
+
+        warningBox.innerHTML = "<h3>Carrinho vázio</h3><span>Adicione itens</span>";
+        extendCart("220");
+        cart.appendChild(warningBox);
+    } 
+    else if (countProduct === 1 && document.querySelector(".warning-cart")) {
+        document.querySelector(".warning-cart").remove();
+    }
+}
